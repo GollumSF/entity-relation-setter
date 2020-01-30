596ae670b3039ae84e8c52ac47eae64c17797461
SmeagolWorms4 <smeagolworms4@gmail.com>
2020-01-30 23:56:24 +0100

    test

--- tests/OneToManyAndManyToOneSetterTest.php
+++ tests/OneToManyAndManyToOneSetterTest.php
@@ -5,63 +5,63 @@ namespace GollumSF\EntityRelationSetter;
 use Doctrine\Common\Inflector\Inflector;
 use Doctrine\Common\Persistence\Proxy;
 
-trait OneToManySetter {
+trait OneToManyAndManyToOneSetterTest {
 	
-	protected function oneToManyAdd($value, string $fieldName = null, $targetName = null): self {
-		
-		if ($fieldName === null) {
-			$trace = debug_backtrace();
-			$calledMethod = $trace[1]['function'];
-			$fieldName = lcfirst(substr($calledMethod, 3));
-			$fieldName = Inflector::pluralize($fieldName);
-		}
-		
-		if ($targetName === null) {
-			$class = get_called_class();
-			if ($class instanceof Proxy) {
-				$class = get_parent_class($class);
-			}
-			$targetName = $class;
-			if (($index = strrpos($targetName, '\\')) !== false) {
-				$targetName = substr($targetName, $index + 1);
-			}
-			$targetName = lcfirst($targetName);
-		}
-		
-		$methodSet = 'set'.ucfirst($targetName);
-		if (!$this->$fieldName->contains($value)) {
-			$this->$fieldName->add($value);
-			$value->$methodSet($this);
-		}
-		return $this;
-	}
-	
-	protected function oneToManyRemove($value, string $fieldName = null, $targetName = null): self {
-		
-		if ($fieldName === null) {
-			$trace = debug_backtrace();
-			$calledMethod = $trace[1]['function'];
-			$fieldName = lcfirst(substr($calledMethod, 6));
-			$fieldName = Inflector::pluralize($fieldName);
-		}
-		
-		if ($targetName === null) {
-			$class = get_called_class();
-			if ($class instanceof Proxy) {
-				$class = get_parent_class($class);
-			}
-			$targetName = $class;
-			if (($index = strrpos($targetName, '\\')) !== false) {
-				$targetName = substr($targetName, $index + 1);
-			}
-			$targetName = lcfirst($targetName);
-		}
-		
-		$methodSet = 'set'.ucfirst($targetName);
-		if ($this->$fieldName->contains($value)) {
-			$this->$fieldName->removeElement($value);
-			$value->$methodSet(null);
-		}
-		return $this;
-	}
+//	protected function oneToManyAdd($value, string $fieldName = null, $targetName = null): self {
+//		
+//		if ($fieldName === null) {
+//			$trace = debug_backtrace();
+//			$calledMethod = $trace[1]['function'];
+//			$fieldName = lcfirst(substr($calledMethod, 3));
+//			$fieldName = Inflector::pluralize($fieldName);
+//		}
+//		
+//		if ($targetName === null) {
+//			$class = get_called_class();
+//			if ($class instanceof Proxy) {
+//				$class = get_parent_class($class);
+//			}
+//			$targetName = $class;
+//			if (($index = strrpos($targetName, '\\')) !== false) {
+//				$targetName = substr($targetName, $index + 1);
+//			}
+//			$targetName = lcfirst($targetName);
+//		}
+//		
+//		$methodSet = 'set'.ucfirst($targetName);
+//		if (!$this->$fieldName->contains($value)) {
+//			$this->$fieldName->add($value);
+//			$value->$methodSet($this);
+//		}
+//		return $this;
+//	}
+//	
+//	protected function oneToManyRemove($value, string $fieldName = null, $targetName = null): self {
+//		
+//		if ($fieldName === null) {
+//			$trace = debug_backtrace();
+//			$calledMethod = $trace[1]['function'];
+//			$fieldName = lcfirst(substr($calledMethod, 6));
+//			$fieldName = Inflector::pluralize($fieldName);
+//		}
+//		
+//		if ($targetName === null) {
+//			$class = get_called_class();
+//			if ($class instanceof Proxy) {
+//				$class = get_parent_class($class);
+//			}
+//			$targetName = $class;
+//			if (($index = strrpos($targetName, '\\')) !== false) {
+//				$targetName = substr($targetName, $index + 1);
+//			}
+//			$targetName = lcfirst($targetName);
+//		}
+//		
+//		$methodSet = 'set'.ucfirst($targetName);
+//		if ($this->$fieldName->contains($value)) {
+//			$this->$fieldName->removeElement($value);
+//			$value->$methodSet(null);
+//		}
+//		return $this;
+//	}
 }
